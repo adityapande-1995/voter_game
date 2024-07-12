ee4bda063cfeb55d27d52aa77518e386d7c15a97
Aditya Pande <aditya050995@gmail.com>
2024-07-12 22:13:14 +0000

    Dockerize bot players
    Signed-off-by: Aditya Pande <aditya050995@gmail.com>

--- run_all.sh
+++ run_all.sh
@@ -10,30 +10,14 @@ build_images() {
 start_containers() {
     echo "Starting containers..."
     docker compose up -d
+    sleep 5
     xdg-open http://localhost:3000
-}
-
-# Function to start the spam voting script
-start_spam_script() {
-    echo "Starting spam voting script..."
-    if [ -f "./bot_players/spam_votes.sh" ]; then
-        cd bot_players
-        ./spam_votes.sh &
-        SPAM_PID=$!
-        cd ..
-    else
-        echo "spam_votes.sh not found!"
-        exit 1
-    fi
 }
 
 # Function to stop all running processes and containers
 stop_all() {
     echo "Stopping all processes and containers..."
     docker compose down
-    if [ -n "$SPAM_PID" ] && kill -0 $SPAM_PID 2>/dev/null; then
-        kill $SPAM_PID
-    fi
     exit 0
 }
 
@@ -46,10 +30,8 @@ build_images
 # Start all containers
 start_containers
 
-# Follow backend, frontend, and leaderboard logs in the background
-docker compose logs -f backend frontend leaderboard &
-
-start_spam_script
+# Follow logs of all services in the background
+docker compose logs -f backend frontend leaderboard bot &
 
 # Wait indefinitely to keep script running
 wait
